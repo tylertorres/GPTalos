1f5bf80842e4ed47f3c4a446fb981000c1eb1361
Ty <tylertorresp@gmail.com>
2023-04-19 16:31:41 -0700

    Initial Commit

---
+++ talos/talosApp.swift
@@ -0,0 +1,17 @@
+//
+//  talosApp.swift
+//  talos
+//
+//  Created by Tyler Torres on 4/19/23.
+//
+
+import SwiftUI
+
+@main
+struct talosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
